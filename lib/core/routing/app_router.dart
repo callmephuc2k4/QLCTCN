ddaed1c4752731c3218585391b4b3b7d7b8b33e1
ndpisme137 <nguyenphuc6a15.8@gmail.com>
2025-09-24 23:06:47 +0700

    test luồng chạy để dùng bottomNavigationBar thay cho điều hướng bằng URL

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/features/profile/presentation/pages/profile_page.dart';
 import '../../features/home/presentation/pages/home_page.dart';
-import '../../features/products/presentation/pages/product_list_page.dart';
 
 // Domain/Data/Network wiring tối thiểu ngay trong router cho dễ hiểu.
 // Sau này bạn có thể chuyển sang DI (get_it/Riverpod) rất nhẹ nhàng.
-import '../../core/network/api_client.dart';
-import '../../features/products/data/datasources/product_remote_datasource.dart';
-import '../../features/products/data/repositories/product_repository_impl.dart';
-import '../../features/products/domain/usecases/get_products.dart';
-import '../../features/products/domain/usecases/create_product.dart';
-import '../../features/products/domain/usecases/update_product.dart';
-import '../../features/products/domain/usecases/delete_product.dart';
+// import '../../core/network/api_client.dart';
+// import '../../features/products/data/datasources/product_remote_datasource.dart';
+// import '../../features/products/data/repositories/product_repository_impl.dart';
+// import '../../features/products/domain/usecases/get_products.dart';
+// import '../../features/products/domain/usecases/create_product.dart';
+// import '../../features/products/domain/usecases/update_product.dart';
+// import '../../features/products/domain/usecases/delete_product.dart';
 class AppRouter {
   static Route<dynamic> onGenerateRoute(RouteSettings settings) {
     switch (settings.name) {
@@ -19,20 +19,23 @@ class AppRouter {
 
       case '/products':
         // Khởi tạo các lớp theo layer
-        final apiClient = ApiClient(); // network (mock)
-        final remoteDS = ProductRemoteDataSource(apiClient: apiClient);
-        final repo = ProductRepositoryImpl(remoteDataSource: remoteDS);
-        final getProducts = GetProducts(repo);
-        final createProduct = CreateProduct(repo);
-        final updateProduct = UpdateProduct(repo);
-        final deleteProduct = DeleteProduct(repo);
+        // final apiClient = ApiClient(); // network (mock)
+        // final remoteDS = ProductRemoteDataSource(apiClient: apiClient);
+        // final repo = ProductRepositoryImpl(remoteDataSource: remoteDS);
+        // final getProducts = GetProducts(repo);
+        // final createProduct = CreateProduct(repo);
+        // final updateProduct = UpdateProduct(repo);
+        // final deleteProduct = DeleteProduct(repo);
 
+        // return MaterialPageRoute(
+        //   builder: (_) => ProductListPage(getProducts: getProducts,
+        //   createProduct: createProduct,
+        //   updateProduct: updateProduct,
+        //   deleteProduct: deleteProduct,
+        //   ),
         return MaterialPageRoute(
-          builder: (_) => ProductListPage(getProducts: getProducts,
-          createProduct: createProduct,
-          updateProduct: updateProduct,
-          deleteProduct: deleteProduct,
-          ),
+
+          builder: (_) => const ProfileApp(),
         );
 
       default:
---
+++ lib/features/profile/presentation/pages/profile_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProfileApp extends StatelessWidget {
+  const ProfileApp ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          body: Text('NDP'),
+      ),
+    );
+  }
+
+}
